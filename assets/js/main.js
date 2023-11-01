753254851bdb624540ffc51cf48bf02021188368
Tahir Rafique <121024103+tahir-rafique@users.noreply.github.com>
2023-11-01 19:04:06 +0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,13 @@
-var randomNum1 = Math.floor( Math.random() * 6 ) + 1;   
-// Generate Random Number between (1 -6).
-
-var randomDiceImage = "dice" + randomNum1 + ".png";     
-//  this line of code will select random images form  ( dice1.png - dice6.png ).
-
-var randomImageSource = "./assets/images/" + randomDiceImage;    
-// images/dice1.png - images/dice6.png 
-
-
 //  Selecting 1st random images
+var randomNum1 = Math.floor( Math.random() * 6 ) + 1;   
+var randomDiceImage = "dice" + randomNum1 + ".png";     
+var randomImageSource = "./assets/images/" + randomDiceImage;    
 var img1 = document.querySelectorAll("img")[0];
 img1.setAttribute("src", randomImageSource);
 
-
-
-
-
-var randomNum2 = Math.floor( Math.random() * 6 ) + 1;   
-// Generate Random Number between (1 -6).
-
-var randomDiceImage = "dice" + randomNum2 + ".png";     
-//  this line of code will select random images form  ( dice1.png - dice6.png ).
-
-var randomImageSource = "./assets/images/" + randomDiceImage;    
-// images/dice1.png - images/dice6.png 
-
-
 //  Selecting 2nd random images
+var randomNum2 = Math.floor( Math.random() * 6 ) + 1;   
+var randomDiceImage = "dice" + randomNum2 + ".png";     
+var randomImageSource = "./assets/images/" + randomDiceImage;    
 var img2 = document.querySelectorAll("img")[1];
 img2.setAttribute("src", randomImageSource);
